a203149b2080b94900f811ae7e992bcbb9ff32a4
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2023-03-18 01:22:08 +0200

    added tasks

---
+++ prog1/u01/task15/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+using ld = long double;
+
+/*
+(*) Read in five measured values of a test series and determine the mean value. Use a maximum of 2 variables with fundamental data type for this.
+*/
+const static size_t n = 5;
+
+int main() {
+    ld mean{}, test{};
+    for (int i = 0; i < n; ++i) {
+        cout << "\na[" << i << "] = ";
+        cin >> test;
+        mean += test / n;
+    }
+    cout << "\nthe mean value = " << mean << '\n';
+    return 0;
+}
